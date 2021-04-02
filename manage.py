4deda406f2f65858e7864b450ff643d625f8bc3e
Damian Dobkowski <damian04121@gmail.com>
2021-04-02 15:22:23 +0200

    Manage.py update to return tests results

--- manage.py
+++ manage.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-exit(0)
+import unittest
+import sys
+
+if 'test' in sys.argv:
+    loader = unittest.TestLoader()
+
+    tests_dir = '.'
+    tests = loader.discover(tests_dir)
+
+    runner = unittest.TextTestRunner()
+    success = runner.run(tests).wasSuccessful()
+
+    exit(0 if success else 1)
---
+++ tests/graph_api_tests/test_get.py
@@ -0,0 +1,9 @@
+import graph_api.main as main
+import unittest
+import asyncio
+
+
+class TestGet(unittest.TestCase):
+
+    def test_root(self):
+        self.assertEqual(asyncio.run(main.root()), {"message": "Hello World"})
