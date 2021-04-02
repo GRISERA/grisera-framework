4e14bbd1bde5742d8b1891cdc7365d8233c29757
Damian Dobkowski <70065334+RioluM@users.noreply.github.com>
2021-04-02 14:26:55 +0200

    GRIS-21. Added config file with database information (#10)

---
+++ database_config.py
@@ -0,0 +1,9 @@
+# parameters of graph database used in API
+
+database = {
+    "address": "http://localhost:7474",
+    "name": "neo4j",
+    "commit_path": "/db/{database_name}/tx/commit",
+    "user": "neo4j",
+    "passwd": "grisera",
+}
