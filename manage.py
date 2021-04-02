cb2588328991352d746f21e6dd96d0c101868b0e
Damian Dobkowski <70065334+RioluM@users.noreply.github.com>
2021-04-02 19:36:24 +0200

    Gris 22. Repository preparation (#11)
    * Repository structure change
    
    * Manage.py update to return tests results
    
    * Move database config file
    
    * Updated config.yml
    
    * Manage.py and config.yml update

--- manage.py
+++ manage.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
-exit(0)
+import unittest
+import sys
+
+if 'test' in sys.argv:
+    loader = unittest.TestLoader()
+
+    try:
+        tests_dir = f'tests/{sys.argv[2]}_tests'
+    except IndexError:
+        tests_dir = 'tests'
+    tests = loader.discover(tests_dir)
+
+    runner = unittest.TextTestRunner()
+    success = runner.run(tests).wasSuccessful()
+
+    exit(0 if success else 1)
