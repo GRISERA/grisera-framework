a44052763c32b717c6bc093cef83fc80564ea753
Paulina <Paulina@DESKTOP-BTQND0N>
2021-04-07 21:40:07 +0200

    Development of relationship model, service and router

--- graph_api/relationship/relationship_model.py
+++ graph_api/relationship/relationship_model.py
@@ -1,5 +1,6 @@
-from typing import Set, Optional, Any
+from typing import Set, Optional, Any, List
 from pydantic import BaseModel
+from property.property_model import PropertyIn
 
 
 class RelationshipIn(BaseModel):
@@ -7,14 +8,14 @@ class RelationshipIn(BaseModel):
         Model of relationship to acquire from client
 
         Attributes:
-            start_node (int): Id of node which starts connection
-            end_node (int): Id of node which ends connection
-            name (str): Name of the relationship
+            start_node (Optional[int]): Id of node which starts connection
+            end_node (Optional[int]): Id of node which ends connection
+            name (Optional[str]): Name of the relationship
     """
 
-    start_node: int
-    end_node: int
-    name: str
+    start_node: Optional[int]
+    end_node: Optional[int]
+    name: Optional[str]
 
 
 class RelationshipOut(RelationshipIn):
@@ -23,9 +24,11 @@ class RelationshipOut(RelationshipIn):
 
         Attributes:
             id (Optional[int]): Id of relationship returned from graph database
+            propeties(Optional[List[PropertyIn]]): List of properties of the relationship in the database
             errors (Optional[Any]): Optional errors appeared during query executions
     """
 
     id: Optional[int]
+    properties: Optional[List[PropertyIn]] = None
     errors: Optional[Any] = None
     links: Optional[list] = None
