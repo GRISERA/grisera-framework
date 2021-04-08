ce5dd5112bc6b2a2236751ad0e103de818dbcd74
paulinaborys <62559956+paulinaborys@users.noreply.github.com>
2021-04-08 11:17:50 +0200

    GRIS-26. Development of relationship model, service and router (#18)
    * Development of relationship model, service and router
    
    * small fix
    
    * small fix
    
    Co-authored-by: Paulina <Paulina@DESKTOP-BTQND0N>

--- graph_api/relationship/relationship_router.py
+++ graph_api/relationship/relationship_router.py
@@ -4,6 +4,8 @@ from fastapi_utils.inferring_router import InferringRouter
 from relationship.relationship_model import RelationshipIn, RelationshipOut
 from relationship.relationship_service import RelationshipService
 from hateoas import get_links
+from typing import List
+from property.property_model import PropertyIn
 
 router = InferringRouter()
 
@@ -14,7 +16,7 @@ class RelationshipRouter:
     Class for routing relationships based requests
 
     Attributes:
-        relationship_service (RelationshipService): Service instance for nodes
+        relationship_service (RelationshipService): Service instance for relationships
     """
 
     relationship_service = RelationshipService()
@@ -25,6 +27,20 @@ class RelationshipRouter:
         Create directed and named relationship
         """
         create_response = self.relationship_service.save_relationship(relationship)
+        if create_response.errors is not None:
+            response.status_code = 422
+
+        # add links from hateoas
+        create_response.links = get_links(router)
+
+        return create_response
+
+    @router.post("/relationships/{id}/properties", tags=["relationships"], response_model=RelationshipOut)
+    async def create_relationship_properties(self, id: int, properties: List[PropertyIn], response: Response):
+        """
+        Create properties for relationship with given id
+        """
+        create_response = self.relationship_service.save_properties(id, properties)
         if create_response.errors is not None:
             response.status_code = 422
 
--- graph_api/relationship/relationship_service.py
+++ graph_api/relationship/relationship_service.py
@@ -2,6 +2,8 @@ import requests
 from requests.auth import HTTPBasicAuth
 from database_config import database
 from relationship.relationship_model import RelationshipOut, RelationshipIn
+from property.property_model import PropertyIn
+from typing import List
 
 
 class RelationshipService:
@@ -15,7 +17,7 @@ class RelationshipService:
     database_url = (database["address"] + database["commit_path"]) \
         .replace("{database_name}", database["name"])
     database_auth = HTTPBasicAuth(database["user"], database["passwd"])
-    
+
     def node_exists(self, node_id):
         """
         Check if node exists in the database
@@ -36,9 +38,29 @@ class RelationshipService:
                                  json=commit_body,
                                  auth=self.database_auth).json()
 
-        return len(response['results'][0]['data']) == 1 
-         
-            
+        return len(response['results'][0]['data']) == 1
+
+    def relationship_exist(self, relationship_id):
+        """
+        Check if relationship exists in the database
+
+        Args:
+            relationship_id(int): id of the relationship
+
+        Returns:
+            True - If there is a relationship in the database.
+            False - If there is not a relationship in the database.
+        """
+        check_relationship_statement = f"MATCH ()-[r]->() where id(r) ={relationship_id} return r"
+        commit_body = {
+            "statements": [{"statement": check_relationship_statement}]
+        }
+        response = requests.post(url=self.database_url,
+                                 json=commit_body,
+                                 auth=self.database_auth).json()
+
+        return len(response['results'][0]['data']) == 1
+
     def save_relationship(self, relationship: RelationshipIn):
         """
         Send request to database by its API to create new relationship
@@ -70,3 +92,39 @@ class RelationshipService:
                                      name=relationship.name, errors={"errors": "not matching id"})
 
         return result
+
+    def save_properties(self, id: int, properties: List[PropertyIn]):
+        """
+        Send request to database by its API to create new properties
+
+        Args:
+            id (int): Id of the relationship
+
+            properties (List[PropertyIn]): List of properties for the relationship of given id
+
+        Returns:
+            Result of request as relationship object
+        """
+        if self.relationship_exist(id):
+            create_statement = f"MATCH ()-[r]->() where id(r)={id} set r = $props return r"
+            commit_body = {
+                "statements": [{"statement": create_statement,
+                                "parameters": {
+                                    "props": {
+                                        property.key: property.value for property in properties
+                                    }
+                                }}]
+            }
+            response = requests.post(url=self.database_url,
+                                     json=commit_body,
+                                     auth=self.database_auth).json()
+            if len(response["errors"]) > 0:
+                result = RelationshipOut(errors=response["errors"])
+            else:
+                relationship_id = response["results"][0]["data"][0]["meta"][0]["id"]
+                result = RelationshipOut(id=relationship_id, properties=properties)
+        else:
+            result = RelationshipOut(id=id, errors={"errors": "not matching id"})
+
+        return result
+    
