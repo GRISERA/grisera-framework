5744d93142bd8f8bff179015e155f624c03304e8
Damian Dobkowski <70065334+RioluM@users.noreply.github.com>
2021-04-12 07:17:12 +0200

    GRIS-27. Create graph api config used in grisera api (#21)
    * Create graph api config used in grisera api
    
    * Add port to config file

---
+++ grisera_api/graph_api_config.py
@@ -0,0 +1,6 @@
+# parameters of graph_api used in API
+import os
+
+graph_api_host = os.environ.get('GRAPH_API_HOST') or 'localhost'
+graph_api_port = 18080
+graph_api_address = "http://{}:{}".format(graph_api_host, graph_api_port)
