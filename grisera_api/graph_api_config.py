7a649507852cbb4fe3d26e5c18f5fc39631e08f3
paulinaborys <62559956+paulinaborys@users.noreply.github.com>
2021-04-21 10:51:14 +0200

    Update graph_api_config.py
    fix of port

--- grisera_api/graph_api_config.py
+++ grisera_api/graph_api_config.py
@@ -2,5 +2,5 @@
 import os
 
 graph_api_host = os.environ.get('GRAPH_API_HOST') or 'localhost'
-graph_api_port = 8000
+graph_api_port = 18080
 graph_api_address = "http://{}:{}".format(graph_api_host, graph_api_port)
