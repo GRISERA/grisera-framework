e706a43f53ec2082dfcb38005ed83e3a1f2bee0e
mrwrob <mrwrob@gmail.com>
2021-04-21 21:02:42 +0200

    GRIS-41. Add  name and description for APIs (#28)

--- grisera_api/main.py
+++ grisera_api/main.py
@@ -3,8 +3,10 @@ from hateoas import get_links
 from participant.participant_router import router as participant_router
 from experiment.experiment_router import router as experiment_router
 
-app = FastAPI()
-
+app = FastAPI(title="GRISERA API",
+              description="Graph Representation Integrating Signals for Emotion Recognition and Analysis (GRISERA) framework provides a persistent model for storing integrated signals and methods for its creation.",
+              version="0.1",
+              )
 app.include_router(participant_router)
 app.include_router(experiment_router)
 
