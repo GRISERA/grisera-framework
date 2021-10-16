9b74d1ad0ffe3b8f93b54291ac4e0a256aae01c9
RioluM <damian04121@gmail.com>
2021-10-17 01:34:32 +0200

    Add delete, update and gets for Participant State

--- grisera_api/participant_state/participant_state_model.py
+++ grisera_api/participant_state/participant_state_model.py
@@ -2,7 +2,7 @@ from typing import List
 from pydantic import BaseModel
 from typing import Optional, Any
 from property.property_model import PropertyIn
-from participant.participant_model import ParticipantIn, ParticipantOut
+from models.relation_information_model import RelationInformation
 
 
 class ParticipantStateIn(BaseModel):
@@ -10,30 +10,84 @@ class ParticipantStateIn(BaseModel):
     Model of participant state to acquire from client
 
     Attributes:
-        participant (Optional[ParticipantIn]): Participant whose state is described
         age (Optional[int]): Age of participant state
-        personality_id (Optional[int]): Id of personality describing participant
-        appearance_id (Optional[int]): Id of appearance describing participant
         additional_properties (Optional[List[PropertyIn]]): Additional properties for participant state
     """
-    participant: Optional[ParticipantIn]
     age: Optional[int]
-    personality_id: Optional[int] = None
-    appearance_id: Optional[int] = None
     additional_properties: Optional[List[PropertyIn]]
 
 
-class ParticipantStateOut(ParticipantStateIn):
+class ParticipantStateRelationIn(ParticipantStateIn):
+    """
+    Model of participant state with relations to acquire from client
+
+    Attributes:
+        participant_id (Optional[int]): Participant whose state is described
+        personality_id (Optional[int]): Id of personality describing participant
+        appearance_id (Optional[int]): Id of appearance describing participant
+    """
+    participant_id: Optional[int]
+    personality_id: Optional[int] = None
+    appearance_id: Optional[int] = None
+
+
+class BasicParticipantStateRelationOut(ParticipantStateRelationIn):
+    """
+    Basic model of participant state with relations
+
+    Attributes:
+        id (Optional[int]): Id of participant returned from graph api
+    """
+    id: Optional[int]
+
+
+class BasicParticipantStateOut(ParticipantStateIn):
+    """
+    Basic model of participant
+
+    Attributes:
+        id (Optional[int]): Id of participant returned from graph api
+    """
+    id: Optional[int]
+
+
+class ParticipantStateOut(BasicParticipantStateRelationOut):
     """
     Model of participant state to send to client as a result of request
 
     Attributes:
-        participant (Optional[ParticipantOut]): Participant whose state is described
-        id (Optional[int]): Id of participant state returned from graph api
         errors (Optional[Any]): Optional errors appeared during query executions
         links (Optional[list]): List of links available from api
     """
-    participant: Optional[ParticipantOut]
-    id: Optional[int]
+    errors: Optional[Any] = None
+    links: Optional[list] = None
+
+
+class ParticipantStateRelationOut(BasicParticipantStateOut):
+    """
+    Model of participant state with relations to send to client as a result of request
+
+    Attributes:
+        relations (List[RelationInformation]): List of relations starting in participant state node
+        reversed_relations (List[RelationInformation]): List of relations ending in participant state node
+        errors (Optional[Any]): Optional errors appeared during query executions
+        links (Optional[list]): List of links available from api
+    """
+    relations: List[RelationInformation] = []
+    reversed_relations: List[RelationInformation] = []
+    errors: Optional[Any] = None
+    links: Optional[list] = None
+
+
+class ParticipantStatesOut(BaseModel):
+    """
+    Model of participant states to send to client as a result of request
+
+    Attributes:
+        participant_states (List[BasicParticipantStateOut]): Participant states from database
+        errors (Optional[Any]): Optional errors appeared during query executions
+        links (Optional[list]): List of links available from api
+    """
+    participant_states: List[BasicParticipantStateOut] = []
     errors: Optional[Any] = None
     links: Optional[list] = None
