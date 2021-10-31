5c7bad9d18b231d936dd5238be37da936cf95598
Damian Dobkowski <damian04121@gmail.com>
2021-10-31 22:31:58 +0100

    Add get, update and delete endpoints for Experiment node

---
+++ grisera_api/tests/tests_experiment/test_experiment_router_delete.py
@@ -0,0 +1,38 @@
+import asyncio
+import unittest
+import unittest.mock as mock
+from experiment.experiment_router import *
+from property.property_model import PropertyIn
+
+
+class TestExperimentRouterDelete(unittest.TestCase):
+
+    @mock.patch.object(ExperimentService, 'delete_experiment')
+    def test_delete_experiment_without_error(self, delete_experiment_mock):
+        experiment_id = 1
+        delete_experiment_mock.return_value = ExperimentOut(experiment_name="test", id=experiment_id,
+                                                            additional_properties=[PropertyIn(key="test", value="test")])
+        response = Response()
+        experiment_router = ExperimentRouter()
+
+        result = asyncio.run(experiment_router.delete_experiment(experiment_id, response))
+
+        self.assertEqual(result, ExperimentOut(experiment_name="test", id=experiment_id,
+                                               additional_properties=[PropertyIn(key="test", value="test")],
+                                               links=get_links(router)))
+        delete_experiment_mock.assert_called_once_with(experiment_id)
+        self.assertEqual(response.status_code, 200)
+
+    @mock.patch.object(ExperimentService, 'delete_experiment')
+    def test_delete_experiment_with_error(self, delete_experiment_mock):
+        delete_experiment_mock.return_value = ExperimentOut(experiment_name="test", errors={'errors': ['test']})
+        response = Response()
+        experiment_id = 1
+        experiment_router = ExperimentRouter()
+
+        result = asyncio.run(experiment_router.delete_experiment(experiment_id, response))
+
+        self.assertEqual(result, ExperimentOut(experiment_name="test", errors={'errors': ['test']},
+                                               links=get_links(router)))
+        delete_experiment_mock.assert_called_once_with(experiment_id)
+        self.assertEqual(response.status_code, 404)
---
+++ grisera_api/tests/tests_experiment/test_experiment_service_get.py
@@ -0,0 +1,92 @@
+import unittest
+import unittest.mock as mock
+
+from experiment.experiment_model import *
+from models.not_found_model import *
+
+from experiment.experiment_service import ExperimentService
+from graph_api_service import GraphApiService
+
+
+class TestExperimentServiceGet(unittest.TestCase):
+
+    @mock.patch.object(GraphApiService, 'get_node')
+    @mock.patch.object(GraphApiService, 'get_node_relationships')
+    def test_get_experiment_without_error(self, get_node_relationships_mock, get_node_mock):
+        id_node = 1
+        get_node_mock.return_value = {'id': id_node, 'labels': ['Experiment'],
+                                      'properties': [{'key': 'experiment_name', 'value': 'test'},
+                                                     {'key': 'test', 'value': 'test'}],
+                                      "errors": None, 'links': None}
+        get_node_relationships_mock.return_value = {"relationships": [
+                                                    {"start_node": id_node, "end_node": 19,
+                                                     "name": "testRelation", "id": 0,
+                                                     "properties": None},
+                                                    {"start_node": 15, "end_node": id_node,
+                                                     "name": "testReversedRelation", "id": 0,
+                                                     "properties": None}]}
+        additional_properties = [PropertyIn(key='test', value="test")]
+        experiment = ExperimentOut(experiment_name="test", additional_properties=additional_properties,
+                                   relations=[RelationInformation(second_node_id=19, name="testRelation",
+                                                                  relation_id=0)],
+                                   reversed_relations=[RelationInformation(second_node_id=15,
+                                                                           name="testReversedRelation", relation_id=0)])
+        experiment_service = ExperimentService()
+
+        result = experiment_service.get_experiment(id_node)
+
+        self.assertEqual(result, experiment)
+        get_node_mock.assert_called_once_with(id_node)
+        get_node_relationships_mock.assert_called_once_with(id_node)
+
+    @mock.patch.object(GraphApiService, 'get_node')
+    def test_get_experiment_without_participant_label(self, get_node_mock):
+        id_node = 1
+        get_node_mock.return_value = {'id': id_node, 'labels': ['Test'], 'properties': None,
+                                      "errors": None, 'links': None}
+        not_found = NotFoundByIdModel(id=id_node, errors="Node not found.")
+        experiment_service = ExperimentService()
+
+        result = experiment_service.get_experiment(id_node)
+
+        self.assertEqual(result, not_found)
+        get_node_mock.assert_called_once_with(id_node)
+
+    @mock.patch.object(GraphApiService, 'get_node')
+    def test_get_experiment_with_error(self, get_node_mock):
+        id_node = 1
+        get_node_mock.return_value = {'id': id_node, 'errors': ['error'], 'links': None}
+        not_found = NotFoundByIdModel(id=id_node, errors=['error'])
+        experiment_service = ExperimentService()
+
+        result = experiment_service.get_experiment(id_node)
+
+        self.assertEqual(result, not_found)
+        get_node_mock.assert_called_once_with(id_node)
+
+    @mock.patch.object(GraphApiService, 'get_nodes')
+    def test_get_experiments(self, get_nodes_mock):
+        get_nodes_mock.return_value = {'nodes': [{'id': 1, 'labels': ['Experiment'],
+                                                  'properties': [{'key': 'experiment_name', 'value': 'test'}]},
+                                                 {'id': 2, 'labels': ['Experiment'],
+                                                  'properties': [{'key': 'experiment_name', 'value': 'test2'}]}]}
+        experiment_one = BasicExperimentOut(experiment_name="test")
+        experiment_two = BasicExperimentOut(experiment_name="test2")
+        experiments = ExperimentsOut(experiments=[experiment_one, experiment_two])
+        experiment_service = ExperimentService()
+
+        result = experiment_service.get_experiments()
+
+        self.assertEqual(result, experiments)
+        get_nodes_mock.assert_called_once_with("Experiment")
+
+    @mock.patch.object(GraphApiService, 'get_nodes')
+    def test_get_experiments_empty(self, get_nodes_mock):
+        get_nodes_mock.return_value = {'nodes': []}
+        experiments = ExperimentsOut(experiments=[])
+        experiment_service = ExperimentService()
+
+        result = experiment_service.get_experiments()
+
+        self.assertEqual(result, experiments)
+        get_nodes_mock.assert_called_once_with("Experiment")
