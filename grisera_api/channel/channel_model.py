6e971668b3367013eda8908b06329e45fb2b572c
Damian Dobkowski <damian04121@gmail.com>
2021-11-01 14:46:33 +0100

    Add get endpoints for Channel nodes

--- grisera_api/channel/channel_model.py
+++ grisera_api/channel/channel_model.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional, Any, List
 from enum import Enum
+from models.relation_information_model import RelationInformation
 
 
 class Type(str, Enum):
@@ -36,7 +37,7 @@ class ChannelIn(BaseModel):
     Model of channel
 
     Attributes:
-        type (str): Type of the channel
+    type (str): Type of the channel
     """
     type: str
 
@@ -46,7 +47,7 @@ class BasicChannelOut(ChannelIn):
     Model of channel in database
 
     Attributes:
-        id (Optional[int]): Id of channel returned from graph api
+    id (Optional[int]): Id of channel returned from graph api
     """
     id: Optional[int]
 
@@ -56,9 +57,13 @@ class ChannelOut(BasicChannelOut):
     Model of channel to send to client as a result of request
 
     Attributes:
-        errors (Optional[Any]): Optional errors appeared during query executions
-        links (Optional[list]): List of links available from api
+    relations (List[RelationInformation]): List of relations starting in registered data node
+    reversed_relations (List[RelationInformation]): List of relations ending in registered data node
+    errors (Optional[Any]): Optional errors appeared during query executions
+    links (Optional[list]): List of links available from api
     """
+    relations: List[RelationInformation] = []
+    reversed_relations: List[RelationInformation] = []
     errors: Optional[Any] = None
     links: Optional[list] = None
 
@@ -68,9 +73,9 @@ class ChannelsOut(BaseModel):
     Model of channels to send to client as a result of request
 
     Attributes:
-        channels (List[BasicChannelOut]): Channels from database
-        errors (Optional[Any]): Optional errors appeared during query executions
-        links (Optional[list]): List of links available from api
+    channels (List[BasicChannelOut]): Channels from database
+    errors (Optional[Any]): Optional errors appeared during query executions
+    links (Optional[list]): List of links available from api
     """
     channels: List[BasicChannelOut] = []
     errors: Optional[Any] = None
