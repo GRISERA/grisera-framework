3d508c8d2fafef57cd7aa7ab0dc9f0c195222e1a
Damian Dobkowski <damian04121@gmail.com>
2021-11-02 16:42:13 +0100

    Setup time fix

--- grisera_api/setup.py
+++ grisera_api/setup.py
@@ -24,7 +24,7 @@ class SetupNodes:
         activity_service = ActivityService()
         if not os.path.exists("lock_activities"):
             open("lock_activities", "w").write("Busy")
-            sleep(20)
+            sleep(40)
             created_activities = [activity.activity for activity in activity_service.get_activities().activities]
             [activity_service.save_activity(ActivityIn(activity=activity_activity.value))
              for activity_activity in Activity
@@ -38,7 +38,7 @@ class SetupNodes:
         channel_service = ChannelService()
         if not os.path.exists("lock_channels"):
             open("lock_channels", "w").write("Busy")
-            sleep(20)
+            sleep(40)
             created_types = [channel.type for channel in channel_service.get_channels().channels]
             [channel_service.save_channel(ChannelIn(type=channel_type.value))
              for channel_type in Type
@@ -52,7 +52,7 @@ class SetupNodes:
         modality_service = ModalityService()
         if not os.path.exists("lock_modalities"):
             open("lock_modalities", "w").write("Busy")
-            sleep(20)
+            sleep(40)
             created_modalities = [modality.modality for modality in modality_service.get_modalities().modalities]
             [modality_service.save_modality(ModalityIn(modality=modality_modality.value))
              for modality_modality in Modality
@@ -66,7 +66,7 @@ class SetupNodes:
         live_activity_service = LiveActivityService()
         if not os.path.exists("lock_live_activities"):
             open("lock_live_activities", "w").write("Busy")
-            sleep(20)
+            sleep(40)
             created_types = [live_activity.live_activity for live_activity in
                              live_activity_service.get_live_activities().live_activities]
 
@@ -82,7 +82,7 @@ class SetupNodes:
         if not os.path.exists("lock_measure_names"):
             measure_name_service = MeasureNameService()
             open("lock_measure_names", "w").write("Busy")
-            sleep(20)
+            sleep(40)
             created_names = [measure_name.name for measure_name in
                              measure_name_service.get_measure_names().measure_names]
             [measure_name_service.save_measure_name(
