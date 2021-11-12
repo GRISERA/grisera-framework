86afbd926a5be0f87529649bc5141650d82f7895
RioluM <damian04121@gmail.com>
2021-11-12 04:55:06 +0100

    Add delete, get, update TimeSeries endpoints

--- grisera_api/time_series/time_series_router.py
+++ grisera_api/time_series/time_series_router.py
@@ -2,8 +2,11 @@ from fastapi import Response
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
 from hateoas import get_links
-from time_series.time_series_model import TimeSeriesIn, TimeSeriesOut
+from time_series.time_series_model import TimeSeriesIn, TimeSeriesNodesOut, TimeSeriesOut, \
+    TimeSeriesPropertyIn, TimeSeriesRelationIn
 from time_series.time_series_service import TimeSeriesService
+from typing import Union
+from models.not_found_model import NotFoundByIdModel
 
 router = InferringRouter()
 
@@ -11,18 +14,19 @@ router = InferringRouter()
 @cbv(router)
 class TimeSeriesRouter:
     """
-    Class for routing time_series based requests
+    Class for routing time series based requests
 
     Attributes:
-        time_series_service (TimeSeriesService): Service instance for time_series
+        time_series_service (TimeSeriesService): Service instance for time series
     """
     time_series_service = TimeSeriesService()
 
     @router.post("/time_series", tags=["time series"], response_model=TimeSeriesOut)
     async def create_time_series(self, time_series: TimeSeriesIn, response: Response):
         """
-        Create time_series in database
+        Create time series in database
         """
+
         create_response = self.time_series_service.save_time_series(time_series)
         if create_response.errors is not None:
             response.status_code = 422
@@ -31,3 +35,81 @@ class TimeSeriesRouter:
         create_response.links = get_links(router)
 
         return create_response
+
+    @router.get("/time_series", tags=["time series"], response_model=TimeSeriesNodesOut)
+    async def get_time_series_nodes(self, response: Response):
+        """
+        Get time series nodes from database
+        """
+
+        get_response = self.time_series_service.get_time_series_nodes()
+
+        # add links from hateoas
+        get_response.links = get_links(router)
+
+        return get_response
+
+    @router.get("/time_series/{time_series_id}", tags=["time series"],
+                response_model=Union[TimeSeriesOut, NotFoundByIdModel])
+    async def get_time_series(self, time_series_id: int, response: Response):
+        """
+        Get time series from database
+        """
+
+        get_response = self.time_series_service.get_time_series(time_series_id)
+        if get_response.errors is not None:
+            response.status_code = 404
+
+        # add links from hateoas
+        get_response.links = get_links(router)
+
+        return get_response
+
+    @router.delete("/time_series/{time_series_id}", tags=["time series"],
+                   response_model=Union[TimeSeriesOut, NotFoundByIdModel])
+    async def delete_time_series(self, time_series_id: int, response: Response):
+        """
+        Delete time series from database
+        """
+        get_response = self.time_series_service.delete_time_series(time_series_id)
+        if get_response.errors is not None:
+            response.status_code = 404
+
+        # add links from hateoas
+        get_response.links = get_links(router)
+
+        return get_response
+
+    @router.put("/time_series/{time_series_id}", tags=["time series"],
+                response_model=Union[TimeSeriesOut, NotFoundByIdModel])
+    async def update_time_series(self, time_series_id: int, time_series: TimeSeriesPropertyIn,
+                                       response: Response):
+        """
+        Update time series model in database
+        """
+        update_response = self.time_series_service.update_time_series(time_series_id,
+                                                                                  time_series)
+        if update_response.errors is not None:
+            response.status_code = 404
+
+        # add links from hateoas
+        update_response.links = get_links(router)
+
+        return update_response
+
+    @router.put("/time_series/{time_series_id}/relationships", tags=["time series"],
+                response_model=Union[TimeSeriesOut, NotFoundByIdModel])
+    async def update_time_series_relationships(self, time_series_id: int,
+                                                     time_series: TimeSeriesRelationIn, response: Response):
+        """
+        Update time series relations in database
+        """
+        update_response = self.time_series_service.update_time_series_relationships(time_series_id,
+                                                                                                time_series)
+        if update_response.errors is not None:
+            response.status_code = 404
+
+        # add links from hateoas
+        update_response.links = get_links(router)
+
+        return update_response
