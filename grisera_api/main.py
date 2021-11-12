03ba0dc9c257e077dbbbc555ab67d0617ea2a0f4
RioluM <damian04121@gmail.com>
2021-11-12 05:18:47 +0100

    Delete nodes not contained at ontology

--- grisera_api/main.py
+++ grisera_api/main.py
@@ -2,7 +2,6 @@ from activity.activity_router import router as activity_router
 from activity_execution.activity_execution_router import router as activity_execution_router
 from arrangement.arrangement_router import router as arrangement_router
 from appearance.appearance_router import router as appearance_router
-from author.author_router import router as author_router
 from channel.channel_router import router as channel_router
 from experiment.experiment_router import router as experiment_router
 from fastapi import FastAPI
@@ -15,7 +14,6 @@ from participant.participant_router import router as participant_router
 from participant_state.participant_state_router import router as participant_state_router
 from participation.participation_router import router as participation_router
 from personality.personality_router import router as personality_router
-from publication.publication_router import router as publication_router
 from recording.recording_router import router as recording_router
 from registered_channel.registered_channel_router import router as registered_channel_router
 from time_series.time_series_router import router as time_series_router
@@ -35,7 +33,6 @@ app.include_router(activity_router)
 app.include_router(activity_execution_router)
 app.include_router(appearance_router)
 app.include_router(arrangement_router)
-app.include_router(author_router)
 app.include_router(channel_router)
 app.include_router(experiment_router)
 app.include_router(live_activity_router)
@@ -47,7 +44,6 @@ app.include_router(participant_router)
 app.include_router(participant_state_router)
 app.include_router(participation_router)
 app.include_router(personality_router)
-app.include_router(publication_router)
 app.include_router(recording_router)
 app.include_router(registered_channel_router)
 app.include_router(registered_data_router)
