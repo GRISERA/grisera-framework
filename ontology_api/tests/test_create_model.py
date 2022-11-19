cd8b7f57b50d4ec7a27ed2c4730ff264dc13a500
KKucht <krzysztof.k2001.01@wp.pl>
2022-11-19 19:43:17 +0100

    Added implementation

---
+++ ontology_api/tests/test_create_model.py
@@ -0,0 +1,39 @@
+import asyncio
+import unittest
+import unittest.mock as mock
+
+from functions.functions import create_model
+from functions.functions import models
+
+
+class CreateModelTestCase(unittest.TestCase):
+
+    @mock.patch('functions.functions','create_model')
+    def test_create_model_without_error(self):
+        my_path = "D:\\owlAC_testParticipant.owl"
+        models.clear()
+
+        result = create_model(my_path)
+
+        self.assertEqual(result , 0)
+
+    @mock.patch('functions.functions', 'create_model')
+    def test_create_model_with_error(self):
+        my_path = "D:\\xd.owl"
+
+        result = create_model(my_path)
+
+        self.assertEqual(result, "Cannot open file")
+
+    @mock.patch('functions.functions', 'create_model')
+    def test_create_model_without_error_add_more(self):
+        my_path = "D:\\owlAC_testParticipant.owl"
+        models.clear()
+        create_model(my_path)
+        create_model(my_path)
+        create_model(my_path)
+        create_model(my_path)
+
+        result = create_model(my_path)
+
+        self.assertEqual(result, 4)
