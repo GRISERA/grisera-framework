bb6146f039fb618eeaf995c39e7ddbb473bd5f6b
migowskiD <dawid.migowski@gmail.com>
2022-12-01 17:22:01 +0100

    Tests dir changes revoked

--- grisera_api/manage.py
+++ grisera_api/manage.py
@@ -5,8 +5,7 @@ import sys
 if 'test' in sys.argv:
     loader = unittest.TestLoader()
     tests_dir = 'tests'
-    test_dir_GraphDB = '/tests_graphdb'
-    tests = loader.discover(tests_dir + test_dir_GraphDB)
+    tests = loader.discover(tests_dir)
 
     runner = unittest.TextTestRunner()
     success = runner.run(tests).wasSuccessful()
