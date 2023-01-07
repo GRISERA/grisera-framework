90c98904a53e02c806b188dc75c309f4255c4098
PLangowski <pawell092000@gmail.com>
2023-01-07 15:55:01 +0100

    Added Ontology API service to GRISERA API

---
+++ grisera_api/ontology_api_service.py
@@ -0,0 +1,50 @@
+import requests
+from ontology_api_config import ontology_api_address
+
+
+class OntologyApiService:
+    """
+    Object that handles communication with Ontology API
+
+    Attributes:
+        ontology_api_url (str): Ontology API URL
+    """
+    ontology_api_url = ontology_api_address
+
+    def post(self, url_part, request_body):
+        """
+        Send a POST request to Ontology API
+        Args:
+            url_part (str): Part to add at the end of the url
+            request_body:
+
+        Returns: Request result
+        """
+        response = requests.post(url=self.ontology_api_url + url_part,
+                                 json=request_body).json()
+        return response
+
+    def get(self, url_part, params):
+        """
+        Send a GET request to Ontology API
+        Args:
+            url_part (str): Part to add at the end of the url
+            params (dict): Request parameters
+
+        Returns: Request result
+        """
+        response = requests.get(url=self.ontology_api_url + url_part,
+                                params=params).json()
+        return response
+
+    def add_instance(self, model_id, class_name):
+        """
+        Send a request to add an instance to Ontology API
+        Args:
+            model_id (str): ID of the model to which the instance is to be added
+            class_name (str): Name of the class of the instance
+
+        Returns: Request result
+        """
+        request_body = {}
+        return self.post(f"model/{model_id}/class/{class_name}/instance", request_body)
