de4a9be6896700b9ba3c999548db330bdffcab89
Piotr Kortas <piokor123@gmail.com>
2023-02-19 22:26:42 +0100

    New models

---
+++ grisera_api/activity/activity_model_new.py
@@ -0,0 +1,64 @@
+from typing import Optional, Union, List
+from enum import Enum
+
+from pydantic import BaseModel
+
+from activity_execution.activity_execution_model_new import ActivityExecutionOut
+from models.base_model_out import BaseModelOut
+from property.property_model import PropertyIn
+
+
+class Activity(str, Enum):
+    """
+    The type of activity
+
+    Attributes:
+    individual (str): Individual activity
+    two_people (str): Two people activity
+    group (str): Group activity
+    """
+    individual = "individual"
+    two_people = "two-people"
+    group = "group"
+
+
+class ActivityIn(BaseModel):
+    """
+    Model of activity
+
+    Attributes:
+    activity (str): Type of activity
+    additional_properties (Optional[List[PropertyIn]]): Additional properties for activity
+    """
+    activity: str
+    additional_properties: Optional[List[PropertyIn]]
+
+
+class BasicActivityOut(ActivityIn):
+    """
+    Model of activity in database
+
+    Attributes:
+    id (Optional[Union[int, str]]): Id of activity returned from api
+    """
+    id: Optional[Union[int, str]]
+
+
+class ActivityOut(BasicActivityOut, BaseModelOut):
+    """
+    Model of activity to send to client as a result of request
+
+    Attributes:
+    activity_executions (Optional[ActivityExecutionOut]): activity_executions related to this activity
+    """
+    activity_executions: Optional[ActivityExecutionOut] = []
+
+
+class ActivitiesOut(BaseModelOut):
+    """
+    Model of activities to send to client as a result of request
+
+    Attributes:
+    activity_types (List[BasicActivityOut]): Activity types from database
+    """
+    activities: List[BasicActivityOut] = []
---
+++ grisera_api/participant_state/participant_state_model_new.py
@@ -0,0 +1,86 @@
+from typing import List
+from typing import Optional, Union
+
+from pydantic import BaseModel
+
+from appearance.appearance_model_new import AppearanceSomatotypeOut, AppearanceOcclusionOut
+from participation.participation_model_new import ParticipationOut
+from participant.participant_model_new import ParticipantOut
+from personality.personality_model_new import PersonalityBigFiveOut, PersonalityPanasOut
+from property.property_model import PropertyIn
+from models.base_model_out import BaseModelOut
+
+
+class ParticipantStatePropertyIn(BaseModel):
+    """
+    Model of participant state to acquire from client
+
+    Attributes:
+        age (Optional[int]): Age of participant state
+        additional_properties (Optional[List[PropertyIn]]): Additional properties for participant state
+    """
+
+    age: Optional[int]
+    additional_properties: Optional[List[PropertyIn]]
+
+
+class ParticipantStateRelationIn(BaseModel):
+    """
+    Model of participant state relations to acquire from client
+
+    Attributes:
+        participant_id (Optional[int]): Participant whose state is described
+        personality_id (Optional[int]): Id of personality describing participant
+        appearance_id (Optional[int]): Id of appearance describing participant
+    """
+
+    participant_id: Optional[Union[int, str]] = None
+    personality_id: Optional[Union[int, str]] = None
+    appearance_id: Optional[Union[int, str]] = None
+
+
+class ParticipantStateIn(ParticipantStatePropertyIn, ParticipantStateRelationIn):
+    """
+    Full model of participant state to acquire from client
+    """
+
+
+class BasicParticipantStateOut(ParticipantStatePropertyIn):
+    """
+    Basic model of participant
+
+    Attributes:
+        id (Optional[int]): Id of participant returned from api
+    """
+
+    id: Optional[Union[int, str]]
+
+
+class ParticipantStateOut(BasicParticipantStateOut, BaseModelOut):
+    """
+    Model of participant state with optional related fields to send to client as a result of request
+
+    Attributes:
+        participations (Optional[List[BasicParticipationOut]]): participations with this participant state
+        participant (Optional[BasicParticipantOut]): participant related to this participant state
+        appearance (Optional[Union[BasicAppearanceSomatotypeOut, BasicAppearanceOcclusionOut]]): appearance related to
+            this participant state
+        personality (Optional[Union[BasicPersonalityBigFiveOut, BasicPersonalityPanasOut]]): personality related to this
+            participant state
+    """
+
+    participations: Optional[List[ParticipationOut]] = None
+    participant: Optional[ParticipantOut] = None
+    appearance: Optional[Union[AppearanceSomatotypeOut, AppearanceOcclusionOut]] = None
+    personality: Optional[Union[PersonalityBigFiveOut, PersonalityPanasOut]] = None
+
+
+class ParticipantStatesOut(BaseModelOut):
+    """
+    Model of participant states to send to client as a result of request
+
+    Attributes:
+        participant_states (List[BasicParticipantStateOut]): Participant states from database
+    """
+
+    participant_states: List[BasicParticipantStateOut] = []
