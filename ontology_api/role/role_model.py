d37f291c7ad8c40682b380157eb73fb7c72f9d0e
PLangowski <pawell092000@gmail.com>
2023-03-05 15:03:12 +0100

    Added role directory containing the model, router nd service files

---
+++ ontology_api/role/role_model.py
@@ -0,0 +1,20 @@
+from typing import Optional, Any
+from pydantic import BaseModel
+
+
+class MinimalRoleModelIn(BaseModel):
+    role_name: Optional[str]
+    src_instance_name: Optional[str]
+
+class ObjectPropertyRoleModelIn(MinimalRoleModelIn):
+    dst_instance_name: Optional[str]
+
+class MinimalRoleModelOut(BaseModel):
+    links: Optional[str]
+    role_name: Optional[str]
+    src_instance_name: Optional[str]
+    errors: Optional[str]
+    
+class ObjectPropertyRoleModelOut(MinimalRoleModelOut):
+    dst_instance_name: Optional[str]
+    
