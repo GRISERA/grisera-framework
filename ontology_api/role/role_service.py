b636a4e029ec0a9ccc78d8d87621bd7b5eefd61e
PLangowski <pawell092000@gmail.com>
2023-03-05 15:30:43 +0100

    POST Object Property role + non-implemented service method

--- ontology_api/role/role_service.py
+++ ontology_api/role/role_service.py
@@ -1,1 +1,20 @@
+from owlready2 import get_ontology
+from role.role_model import ObjectPropertyRoleModelIn
+from role.role_model import ObjectPropertyRoleModelOut
+from model.model_service import ModelService
 
+class RoleService:
+    """
+        Object to handle logic of role requests
+    """
+    def __init__(self):
+        self.model_service = ModelService()
+
+    def add_object_property_role(self, model_id: int, model_in: ObjectPropertyRoleModelIn):
+        """
+                Create an instance of an object property in the given model
+        """
+        onto = self.model_service.load_ontology(model_id)
+        onto.destroy()
+        return ObjectPropertyRoleModelOut()
+        
