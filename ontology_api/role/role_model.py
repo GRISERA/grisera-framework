865bb3da386b1b49534f42b8390f8032c05c8fb5
KKucht <krzysztof.k2001.01@wp.pl>
2023-03-09 23:33:02 +0100

    Add add_datatype_property_role

--- ontology_api/role/role_model.py
+++ ontology_api/role/role_model.py
@@ -6,15 +6,25 @@ class MinimalRoleModelIn(BaseModel):
     role_name: Optional[str]
     src_instance_name: Optional[str]
 
+
 class ObjectPropertyRoleModelIn(MinimalRoleModelIn):
     dst_instance_name: Optional[str]
+
+
+class DataTypePropertyRoleModelIn(MinimalRoleModelIn):
+    value: Optional[Any]
+
 
 class MinimalRoleModelOut(BaseModel):
     links: Optional[str]
     role_name: Optional[str]
     src_instance_name: Optional[str]
     errors: Optional[str]
-    
+
+
 class ObjectPropertyRoleModelOut(MinimalRoleModelOut):
     dst_instance_name: Optional[str]
-    
+
+
+class DataTypePropertyRoleModelOut(MinimalRoleModelOut):
+    value: Optional[Any]
