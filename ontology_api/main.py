df27906808b25f3bee921d1a1a0da269704fb1d7
PLangowski <pawell092000@gmail.com>
2023-03-09 19:18:18 +0100

    modified main.py to include the role router

--- ontology_api/main.py
+++ ontology_api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from hateoas import get_links
 from model.model_router import router as model_router
 from instance.instance_router import router as instance_router
+from role.role_router import router as role_router
 app = FastAPI(title="Ontology API",
               description="Ontology API is reference implementation for creating, manipulating and serialising Ontologies for the GRISERA framework.",
               version="0.1",
@@ -9,7 +10,7 @@ app = FastAPI(title="Ontology API",
 
 app.include_router(model_router)
 app.include_router(instance_router)
-
+app.include_router(role_router)
 @app.get("/", tags=["root"])
 async def root():
     """
