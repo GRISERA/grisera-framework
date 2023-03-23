042f2cf49c1d4a1c000845069452e71caf0cb034
PLangowski <pawell092000@gmail.com>
2023-03-23 15:11:14 +0100

    delete_roles implementation

--- ontology_api/role/role_model.py
+++ ontology_api/role/role_model.py
@@ -7,6 +7,11 @@ class RoleModelIn(BaseModel):
     instance_name: Optional[str]
     value: Union[str, int, None]
 
+class RolesDeletedOut(BaseModel):
+    model_id: Optional[int]
+    instance_name: Optional[str]
+    links: Optional[str]
+    errors: Optional[str]
 
 class RoleModelOut(RoleModelIn):
     links: Optional[str]
