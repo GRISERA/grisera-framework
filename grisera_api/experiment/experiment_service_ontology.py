bc838be7c32f0449047116c7bf7330f414661661
KKucht <krzysztof.k2001.01@wp.pl>
2023-05-08 21:34:27 +0200

    update_experiment

--- grisera_api/experiment/experiment_service_ontology.py
+++ grisera_api/experiment/experiment_service_ontology.py
@@ -1,5 +1,6 @@
 from experiment.experiment_model import ExperimentIn, ExperimentOut
 from experiment.experiment_service import ExperimentService
+from models.not_found_model import NotFoundByIdModel
 from ontology_api_service import OntologyApiService
 
 
@@ -34,3 +35,31 @@ class ExperimentServiceOntology(ExperimentService):
         experiment.__dict__.update({'experiment_name': experiment_label})
 
         return ExperimentOut(**experiment.dict())
+
+    def update_experiment(self, experiment_id: int, experiment: ExperimentIn):
+        """
+        Send request to graph api to update given experiment
+
+        Args:
+        experiment_id (int): Id of experiment
+        experiment (ExperimentIn): Properties to update
+
+        Returns:
+            Result of request as experiment object
+        """
+        model_id = 1
+        get_response = self.get_experiment(experiment_id)
+
+        if type(get_response) is NotFoundByIdModel:
+            return get_response
+
+        self.ontology_api_service.delete_roles(model_id, experiment.experiment_name)
+
+        for prop in experiment.additional_properties:
+            response = self.ontology_api_service.add_role(model_id, prop.key, experiment.experiment_name, prop.value)
+
+        experiment_result = {'id': experiment_id, 'relations': get_response.relations,
+                             'reversed_relations': get_response.reversed_relations}
+        experiment_result.update(experiment.dict())
+
+        return ExperimentOut(**experiment_result)
