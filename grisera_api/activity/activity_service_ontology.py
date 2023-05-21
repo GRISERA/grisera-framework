08e6801355f6f18eb7a77165127073f422b595d9
KKucht <krzysztof.k2001.01@wp.pl>
2023-05-21 14:38:10 +0200

    Create basic funtions for ActivityServiceOntology

--- grisera_api/activity/activity_service_ontology.py
+++ grisera_api/activity/activity_service_ontology.py
@@ -1,5 +1,25 @@
+from typing import Union
+
+from activity.activity_model import ActivityIn
 from activity.activity_service import ActivityService
+from ontology_api_service import OntologyApiService
 
 
 class ActivityServiceOntology(ActivityService):
-    pass
+    """
+    Object to handle logic of activity requests
+
+    Attributes:
+    ontology_api_service (OntologyApiService): Service used to communicate with Ontology API
+    """
+    ontology_api_service = OntologyApiService()
+
+    def save_activity(self, activity: ActivityIn):
+        super().save_activity()
+
+    def get_activities(self):
+        super().get_activities()
+
+    def get_activity(self, activity_id: Union[int, str], depth: int = 0):
+        super().get_activity(activity_id, depth)
+
