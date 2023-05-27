2ec82159ccd21c88ee87ac69388aebc56412f605
PLangowski <pawell092000@gmail.com>
2023-05-27 16:58:24 +0200

    save_activity version 1

--- grisera_api/activity/activity_service_ontology.py
+++ grisera_api/activity/activity_service_ontology.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from activity.activity_model import ActivityIn
+from activity.activity_model import ActivityIn, ActivityOut
 from activity.activity_service import ActivityService
 from ontology_api_service import OntologyApiService
 
@@ -15,7 +15,22 @@ class ActivityServiceOntology(ActivityService):
     ontology_api_service = OntologyApiService()
 
     def save_activity(self, activity: ActivityIn):
-        super().save_activity()
+        """
+        Send request to graph api to create new activity
+
+        Args:
+            activity (ActivityIn): Activity to be added
+
+        Returns:
+            Result of request as activity object
+        """
+        model_id = 1
+        instance_response_activity = self.ontology_api_service.add_instance(model_id, "Activity", "test")
+        if instance_response_activity["errors"] is not None:
+            return ActivityOut(**activity.dict(), errors=instance_response_activity["errors"])
+        activity_label = instance_response_activity["label"]
+        activity.__dict__.update({'activity_name': activity_label})
+        return ActivityOut(**activity.dict())
 
     def get_activities(self):
         super().get_activities()
