0ed63152392512886ca2e061b35f3c1ed5e18520
dideek <piotrgarbowski2@gmail.com>
2023-10-15 15:02:28 +0200

    First service implementation

--- grisera_api/activity/activity_service_relational.py
+++ grisera_api/activity/activity_service_relational.py
@@ -1,5 +1,16 @@
-from activity.activity_service import ActivityService
+from grisera_api.activity.activity_model import BasicActivityOut, ActivitiesOut
+from grisera_api.activity.activity_service import ActivityService
+from grisera_api.rdb_api_service import RdbApiService
+
 
 
 class ActivityServiceRelational(ActivityService):
-    pass
+    rdb_api_service = RdbApiService()
+    table_name = "Activity"
+    def get_activities(self):
+        results = self.rdb_api_service.get(self.table_name)
+        activities = [BasicActivityOut(id=activity["id"], activity=activity["activity"],
+        additional_properties=activity["additional_properties"]) for activity in results]
+        return ActivitiesOut(activities)
+
+
