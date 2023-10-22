fb0d4bdc65a519ab1cd3e647bf45f505783828a5
Noxxxxious <michal.wrzosek.robert@gmail.com>
2023-10-22 07:10:10 +0200

    change post method, union syntax:

--- grisera_api/rdb_api_config.py
+++ grisera_api/rdb_api_config.py
@@ -1,7 +1,7 @@
 import os
 
 rdb_api_host = os.environ.get("RDB_HOST") or "localhost"
-rdb_api_port = os.environ.get("RDB_PORT") or "8000"
+rdb_api_port = os.environ.get("RDB_PORT") or "1234"
 rdb_api_database_name = os.environ.get("RDB_NAME") or "db"
 rdb_api_user = os.environ.get("RDB_USER") or "user"
 rdb_api_password = os.environ.get("RDB_PASSWORD") or "123"
