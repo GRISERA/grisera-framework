b66b2f4252c16477a84ce147eedf52d9d40042e1
Marcin Gesek <marcin.gesek79@gmail.com>
2023-10-23 21:19:32 +0200

    arrangement

--- grisera_api/arrangement/arrangement_service_relational.py
+++ grisera_api/arrangement/arrangement_service_relational.py
@@ -1,5 +1,33 @@
+from typing import Union
 from arrangement.arrangement_service import ArrangementService
+from arrangement.arrangement_model import ArrangementIn, ArrangementOut, ArrangementsOut, BasicArrangementOut
+from models.not_found_model import NotFoundByIdModel
+from rdb_api_service import RdbApiService
 
 
 class ArrangementServiceRelational(ArrangementService):
-    pass
+
+    rdb_api_service = RdbApiService()
+    table_name = "Arrangement"
+
+    def save_arrangement(self, arrangement: ArrangementIn):
+        arrangement_data = {
+            "type": arrangement.arrangement_type,
+            "distance": arrangement.arrangement_distance
+        }
+        saved_arrangement = self.rdb_api_service.post(self.table_name, arrangement_data)
+        return ArrangementOut(id=saved_arrangement["id"], arrangement_type=saved_arrangement["type"], arrangement_distance=saved_arrangement["distance"])
+
+    def get_arrangements(self):
+        results = self.rdb_api_service.get(self.table_name)
+        arrangements = []
+        for arrangement in results:
+            arrangements.append(BasicArrangementOut(id=arrangement["id"], arrangement_type=arrangement["type"], arrangement_distance=arrangement["distance"]))
+        return ArrangementsOut(arrangements=arrangements)
+
+    def get_arrangement(self, arrangement_id: Union[int, str], depth: int = 0):
+        arrangement = self.rdb_api_service.get_with_id(self.table_name, arrangement_id)
+        if not arrangement:
+            return NotFoundByIdModel(id=arrangement_id, errors="Entity not found")
+        return ArrangementOut(id=arrangement["id"], arrangement_type=arrangement["type"], arrangement_distance=arrangement["distance"])
+    
--- grisera_api/rdb_api_config.py
+++ grisera_api/rdb_api_config.py
@@ -1,8 +1,8 @@
 import os
 
 rdb_api_host = os.environ.get("RDB_HOST") or "localhost"
-rdb_api_port = os.environ.get("RDB_PORT") or "8000"
-rdb_api_database_name = os.environt.get("RDB_NAME") or "db"
-rdb_api_user = os.environt.get("RDB_USER") or "user"
-rdb_api_password = os.environt.get("RDB_PASSWORD") or "123"
+rdb_api_port = os.environ.get("RDB_PORT") or "1234"
+rdb_api_database_name = os.environ.get("RDB_NAME") or "db"
+rdb_api_user = os.environ.get("RDB_USER") or "user"
+rdb_api_password = os.environ.get("RDB_PASSWORD") or "123"
 rdb_api_address = f"http://{rdb_api_host}:{rdb_api_port}"
