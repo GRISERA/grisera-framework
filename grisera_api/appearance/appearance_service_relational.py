38c2a8b9be4a2bac2698dd638aaa8bb5797d462d
Noxxxxious <michal.wrzosek.robert@gmail.com>
2023-10-31 09:07:50 +0100

    add arrangement service, appearance service fixes

--- grisera_api/appearance/appearance_service_relational.py
+++ grisera_api/appearance/appearance_service_relational.py
@@ -1,5 +1,5 @@
 from typing import Union
-from appearance.appearance_model import AppearancesOut, BasicAppearanceSomatotypeOut, BasicAppearanceOcclusionOut, AppearanceOcclusionOut, AppearanceSomatotypeOut, AppearanceOcclusionIn, AppearanceSomatotypeIn
+from appearance.appearance_model import AppearancesOut, AppearanceOcclusionOut, AppearanceSomatotypeOut, AppearanceOcclusionIn, AppearanceSomatotypeIn
 from models.not_found_model import NotFoundByIdModel
 from appearance.appearance_service import AppearanceService
 from rdb_api_service import RdbApiService
@@ -13,18 +13,6 @@ class AppearanceServiceRelational(AppearanceService):
 
     def __init__(self):
         self.participant_state_service = ParticipantStateServiceRelational()
-
-    def save_appearance_occlusion(self, appearance: AppearanceOcclusionIn):
-        appearance_data = {
-            "type": "occlusion",
-            "beard": appearance.beard,
-            "moustache": appearance.moustache,
-            "glasses": appearance.glasses
-        }
-
-        saved_appearance_dict = self.rdb_api_service.post(self.table_name, appearance_data)
-
-        return AppearanceOcclusionOut(**saved_appearance_dict)
     
     def save_appearance_somatotype(self, appearance: AppearanceSomatotypeIn):
         if not self.is_valid_somatotype(appearance):
@@ -50,7 +38,6 @@ class AppearanceServiceRelational(AppearanceService):
         if depth > 0:
             if source != "participant_state":
                 appearance_dict["participant_states"] = self.participant_state_service.get_multiple_with_foreign_id(appearance_id, depth - 1, self.table_name)
-        print(appearance_dict)
         return AppearanceOcclusionOut(**appearance_dict) if appearance_dict["type"] == "occlusion" else AppearanceSomatotypeOut(**appearance_dict)
     
     def get_appearances(self):
--- grisera_api/arrangement/arrangement_service_relational.py
+++ grisera_api/arrangement/arrangement_service_relational.py
@@ -1,5 +1,39 @@
+from typing import Union
 from arrangement.arrangement_service import ArrangementService
-
+from arrangement.arrangement_model import ArrangementOut, ArrangementIn, ArrangementsOut
+from models.not_found_model import NotFoundByIdModel
+from rdb_api_service import RdbApiService
+from activity_execution.activity_execution_service_relational import ActivityExecutionServiceRelational
 
 class ArrangementServiceRelational(ArrangementService):
-    pass
+    rdb_api_service = RdbApiService()
+    table_name = "arrangement"
+
+    def __init__(self):
+        self.activity_execution_service = ActivityExecutionServiceRelational()
+
+    def save_arrangement(self, arrangement: ArrangementIn):
+        arrangement_data = {
+            "arrangement_type": arrangement.arrangement_type,
+            "arrangement_distance": arrangement.arrangement_distance
+        }
+
+        saved_arrangement_dict = self.rdb_api_service.post(self.table_name, arrangement_data)
+
+        return ArrangementOut(**saved_arrangement_dict)
+
+    def get_arrangements(self):
+        results = self.rdb_api_service.get(self.table_name)
+        return  ArrangementsOut(arrangements=results)
+    
+    def get_arrangement(self, arrangement_id: Union[int, str], depth: int = 0, source: str = ""):
+        arrangement_dict = self.rdb_api_service.get_with_id(self.table_name, arrangement_id)
+        if not arrangement_dict:
+            return NotFoundByIdModel(id=arrangement_id, errors={"Entity not found."})
+        
+        if depth > 0:
+            if source != "activity_execution":
+                # not implemented yet
+                arrangement_dict["activity_executions"] = self.activity_execution_service.get_single_with_foreign_id(arrangement_id, depth - 1, self.table_name)
+
+        return ArrangementOut(**arrangement_dict)
