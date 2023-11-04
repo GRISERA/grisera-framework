aa4e475fb683d358669537888788c14e6a6f81ba
Marcin Gesek <marcin.gesek79@gmail.com>
2023-11-04 13:57:13 +0100

    added activity service

--- grisera_api/activity/activity_service_relational.py
+++ grisera_api/activity/activity_service_relational.py
@@ -1,30 +1,44 @@
 from typing import Union
-from activity.activity_model import ActivityOut, BasicActivityOut, ActivitiesOut
+from activity.activity_model import ActivityIn, ActivityOut, BasicActivityOut, ActivitiesOut, Activity
 from activity.activity_service import ActivityService
+from models.not_found_model import NotFoundByIdModel
 from property.property_model import PropertyIn
 from rdb_api_service import RdbApiService
 
 
 class ActivityServiceRelational(ActivityService):
 
-    rdb_api_service = RdbApiService()
-    table_name = "Activity"
+    def __init__(self):
+        self.rdb_api_service = RdbApiService()
+        self.table_name = "activity"
+
+    def save_activity(self, activity: ActivityIn):
+        if not self.is_valid_activity(activity):
+            return ActivityOut(activity_name=activity.activity_name, activity=activity.activity, \
+                               additional_properties=activity.additional_properties, \
+                               errors="Invalid activity type")
+
+        activity_data = {
+            "name": activity.activity_name,
+            "activity": activity.activity,
+            "additional_properties": activity.additional_properties
+        }
+        
+        saved_activity_dict = self.rdb_api_service.post(self.table_name, activity_data)
+        return ActivityOut(**saved_activity_dict)
+
+    def get_activity(self, activity_id: int | str, depth: int = 0):
+        activity_dict = self.rdb_api_service.get_with_id(self.table_name, activity_id)
+        if not activity_dict:
+            return NotFoundByIdModel(id=activity_id, errors={"Entity not found."})
+        return ActivityOut(**activity_dict)
 
     def get_activities(self):
         results = self.rdb_api_service.get(self.table_name)
-        activities = []
-        for activity in results:
-            additional_properties = []
-            for k, v in activity["additional_properties"].items():
-                additional_properties.append(PropertyIn(key=k, value=v))
-            activities.append(BasicActivityOut(id=activity["id"], activity=activity["activity"], additional_properties=additional_properties))    
+        return ActivitiesOut(activities=results)
 
-        return ActivitiesOut(activities=activities)
-
-    def get_activity(self, activity_id: int | str, depth: int = 0):
-        activity = self.rdb_api_service.get_with_id(self.table_name, activity_id)
-        additional_properties = []
-        for k, v in activity["additional_properties"].items():
-            additional_properties.append(PropertyIn(key=k, value=v))
-
-        return ActivityOut(id=activity["id"], activity=activity["activity"], additional_properties=additional_properties)
+    def is_valid_activity(self, activity: ActivityIn):
+        return  activity.activity == Activity.individual or \
+                activity.activity == Activity.two_people or  \
+                activity.activity == Activity.group
+    
