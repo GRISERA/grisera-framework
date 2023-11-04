54a341900ced675ea838020677fd1f6c16dc81f3
Marcin Gesek <marcin.gesek79@gmail.com>
2023-11-04 19:12:07 +0100

    added additional_properties handling

--- grisera_api/registered_data/registered_data_service_relational.py
+++ grisera_api/registered_data/registered_data_service_relational.py
@@ -1,3 +1,4 @@
+import json
 from typing import Union
 from registered_channel.registered_channel_service import RegisteredChannelService
 from models.not_found_model import NotFoundByIdModel
@@ -17,7 +18,12 @@ class RegisteredDataServiceRelational(RegisteredDataService):
     def save_registered_data(self, registered_data: RegisteredDataIn):
         registered_data_dict = {
             "source": registered_data.source,
-            "additional_properties": registered_data.additional_properties
+            "additional_properties": json.dumps([
+                {
+                    "key": p.key,
+                    "value": p.value
+                } for p in registered_data.additional_properties
+            ])
         }
         saved_registered_data_dict = self.rdb_api_service.post(self.table_name, registered_data_dict)
         return RegisteredDataOut(**saved_registered_data_dict)
@@ -45,9 +51,19 @@ class RegisteredDataServiceRelational(RegisteredDataService):
 
 
     def update_registered_data(self, registered_data_id: Union[int, str], registered_data: RegisteredDataIn):
+        registered_data_dict = {
+            "source": registered_data.source,
+            "additional_properties": json.dumps([
+                {
+                    "key": p.key,
+                    "value": p.value
+                } for p in registered_data.additional_properties
+            ])
+        }
+
         result = self.get_registered_data(registered_data_id)
         if type(result) != NotFoundByIdModel:
-            self.rdb_api_service.put(self.table_name, registered_data_id, registered_data.dict())
+            self.rdb_api_service.put(self.table_name, registered_data_id, registered_data_dict)
         return self.get_registered_data(registered_data_id)
     
 
