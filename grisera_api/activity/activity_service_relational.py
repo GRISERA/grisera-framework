d687433654ac3183e8e7fafeea064afd0aa70d50
Marcin Gesek <marcin.gesek79@gmail.com>
2023-11-11 22:02:30 +0100

    implementation

--- grisera_api/activity/activity_service_relational.py
+++ grisera_api/activity/activity_service_relational.py
@@ -1,10 +1,8 @@
 import json
 from typing import Union
-from activity.activity_model import ActivityIn, ActivityOut, BasicActivityOut, ActivitiesOut, Activity
+from activity.activity_model import ActivityIn, ActivityOut,  ActivitiesOut
 from activity.activity_service import ActivityService
-from activity_execution.activity_execution_service import ActivityExecutionService
 from models.not_found_model import NotFoundByIdModel
-from property.property_model import PropertyIn
 from rdb_api_service import RdbApiService, Collections
 
 
@@ -12,17 +10,12 @@ class ActivityServiceRelational(ActivityService):
 
     def __init__(self):
         self.rdb_api_service = RdbApiService()
-        self.activity_execution_service = ActivityExecutionService()
         self.table_name = Collections.ACTIVITY
 
-    def save_activity(self, activity: ActivityIn):
-        if not self.is_valid_activity(activity):
-            return ActivityOut(activity_name=activity.activity_name, activity=activity.activity, \
-                               additional_properties=activity.additional_properties, \
-                               errors="Invalid activity type")
 
+    def save_activity(self, activity: ActivityIn):
         activity_dict = {
-            "name": activity.activity_name,
+            "activity_name": activity.activity_name,
             "activity": activity.activity,
             "additional_properties": json.dumps([
                 {
@@ -32,30 +25,26 @@ class ActivityServiceRelational(ActivityService):
             ])
         }
         
-        saved_activity_dict = self.rdb_api_service.post(self.table_name, activity_dict)
+        saved_activity_dict = self.rdb_api_service.post(self.table_name, activity_dict)["records"]
         return ActivityOut(**saved_activity_dict)
 
-    def get_activity(self, activity_id: Union[int, str], depth: int = 0, source: str = ""):
-        activity_dict = self.rdb_api_service.get_with_id(self.table_name, activity_id)
-        if not activity_dict:
-            return NotFoundByIdModel(id=activity_id, errors={"Entity not found."})
-        
-        if depth > 0 and source != Collections.ACTIVITY_EXECUTION:
-            activity_dict["activity_executions"] = self.activity_execution_service.get_multiple_with_foreign_id(activity_id, depth - 1, self.table_name)
-
-        return ActivityOut(**activity_dict)
 
     def get_activities(self):
         results = self.rdb_api_service.get(self.table_name)
         return ActivitiesOut(activities=results)
 
-    def is_valid_activity(self, activity: ActivityIn):
-        return  activity.activity == Activity.individual or \
-                activity.activity == Activity.two_people or  \
-                activity.activity == Activity.group
-    
-    def get_single_with_foreign_id(self, activity_id: Union[int, str], depth: int = 0, source: str = ""):
+
+    def get_activity(self, activity_id: Union[int, str], depth: int = 0, source: str = ""):
+        #import activity_execution.activity_execution_service as ae_rel
+        #activity_execution_service = ae_rel.ActivityExecutionService()
+        
+        activity_dict = self.rdb_api_service.get_with_id(self.table_name, activity_id)
+        if not activity_dict:
+            return NotFoundByIdModel(id=activity_id, errors={"Entity not found."})
+        
         if depth > 0 and source != Collections.ACTIVITY_EXECUTION:
-            result = self.rdb_api_service.get_with_id(self.table_name, activity_id)
-            return result
-        return None
+            #activity_dict["activity_executions"] = activity_execution_service.get_multiple_with_foreign_id(activity_id, depth - 1, self.table_name)
+            pass
+
+        return ActivityOut(**activity_dict)
+
