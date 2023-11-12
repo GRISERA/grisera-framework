c482fc645d70dbbd707c62aaa70e40d2a83bbed7
Noxxxxious <michal.wrzosek.robert@gmail.com>
2023-11-12 18:51:42 +0100

    post method

--- grisera_api/time_series/time_series_service_relational.py
+++ grisera_api/time_series/time_series_service_relational.py
@@ -1,5 +1,28 @@
+import json
+from models.not_found_model import NotFoundByIdModel
+from rdb_api_service import RdbApiService, Collections
+from time_series.time_series_model import TimeSeriesIn, TimeSeriesOut
 from time_series.time_series_service import TimeSeriesService
 
 
 class TimeSeriesServiceRelational(TimeSeriesService):
-    pass
+    
+    def __init__(self) -> None:
+        self.rdb_api_service = RdbApiService()
+        self.table_name = Collections.TIMESERIES
+
+    def save_time_series(self, time_series: TimeSeriesIn):
+        time_series_dict = time_series.dict()
+        if time_series.additional_properties is not None:
+            time_series_dict["additional_properties"] = json.dumps([
+                {
+                    "key": p.key,
+                    "value": p.value
+                } for p in time_series.additional_properties
+            ])
+        print(time_series_dict)
+        saved_time_series_dict = self.rdb_api_service.post(self.table_name, time_series_dict)
+        print(saved_time_series_dict)
+        if saved_time_series_dict["errors"] is not None:
+            return TimeSeriesOut(errors=saved_time_series_dict["errors"])
+        return TimeSeriesOut(**saved_time_series_dict["records"])
